856a7ad58d3a9284fc8e786f183fbca0bfbd3165
Rodrigo Sandeski <sandeski47@gmail.com>
2022-03-24 20:46:09 -0300

    Criada a classe Posicao

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using xadrez_console.tabuleiro;
 
 namespace xadrez_console
 {
@@ -10,6 +7,10 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
+
+            Posicao p = new Posicao(3, 4);
+            Console.WriteLine("Posicao : " + p);
+            Console.ReadKey();
         }
     }
 }
